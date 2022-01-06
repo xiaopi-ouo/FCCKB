540b332318ecda334aa254b629ed4705f1a162a2
r08922129 <r08922129@csie.ntu.edu.tw>
2022-01-07 01:10:25 +0800

    add args.prepend_title_frame

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ parser.add_argument("path_out", help="root directoy for saving result. eg. ./sav
 parser.add_argument("-k", help="to retrieve K claims.", default=100, type=int)
 parser.add_argument("-s", "--strategy", help="simialarity strategy", default="srl", type=str)
 parser.add_argument("-e", "--encoder_name", help="sentence encoder for query sentence encoding", default="sbert", type=str)
-parser.add_argument("-t", "--prepend_title", help="prepend title or not", default=True, type=bool)
+parser.add_argument("-t", "--prepend_title_sentence", help="prepend title to the sentence or not", default=True, type=bool)
+parser.add_argument("-f", "--prepend_title_frame", help="prepend title to frames or not", default=True, type=bool)
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -46,7 +47,10 @@ if __name__ == "__main__":
         raise Exception("Method %s does not exist.".format(arg_method))
 
     retriever = ClaimRetriever(claims, encoder_and_database_factory, similarity_strategy)
-    retrieval_result = retriever.retrieve(samples, k=args.k, prepend_title=args.prepend_title)
+    retrieval_result = retriever.retrieve(samples,
+                                          k=args.k, 
+                                          prepend_title_sentence=args.prepend_title_sentence,
+                                          prepend_title_frame=args.prepend_title_frame)
 
     with open(os.path.join(args.path_out, f"{args.encoder_name}-{args.strategy}.pkl"), "wb") as f:
         pickle.dump(retrieval_result, f)
